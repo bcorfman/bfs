c2729f3d3e0dee7fb2e4136475aa38f0a59012b8
Brandon Corfman <brandon.c.corfman.civ@mail.mil>
2021-09-28 16:25:45 -0400

    FEATURE: add main.py to run our search

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#bfs 
+from app.search import breadth_first_search, GridSearchProblem
+
+
+def main():
+    problem = GridSearchProblem()
+    print(f"Start: {problem.start}")
+    print(f"Goal: {problem.goal}")
+    path = breadth_first_search(problem)
+    print(f"Path: {path}")
+
+
+if __name__ == '__main__':
+    main()
