ac4e8fe9d5db9e90822912ef70d337faa5a84793
Brandon Corfman <bcorfman@fastmail.fm>
2023-04-26 15:08:36 -0400

    reintro if __name__

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import streamlit as st
 
 from core.search import GridSearchProblem, breadth_first_search
 
-st.title("Breadth-first search")
-problem = GridSearchProblem()
-st.write(f"Start: {problem.start}")
-st.write(f"Goal: {problem.goal}")
-path = breadth_first_search(problem)
-st.write(f"Path: {path}")
-st.write(problem.plotSolution(path))
+
+def main():
+    st.title("Breadth-first search")
+    problem = GridSearchProblem()
+    st.write(f"Start: {problem.start}")
+    st.write(f"Goal: {problem.goal}")
+    path = breadth_first_search(problem)
+    st.write(f"Path: {path}")
+    st.write(problem.plotSolution(path))
+
+
+if __name__ == "__main__":
+    main()
