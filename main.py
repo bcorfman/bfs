26207627555d249c1adcb257e58fd432a7654795
Brandon Corfman <bcorfman@fastmail.fm>
2023-04-26 14:55:11 -0400

    remove if __main__

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import streamlit as st
+
 from core.search import GridSearchProblem, breadth_first_search
 
-
-def main():
-    st.header("Breadth-first search")
-    problem = GridSearchProblem()
-    st.write(f"Start: {problem.start}")
-    st.write(f"Goal: {problem.goal}")
-    path = breadth_first_search(problem)
-    st.write(f"Path: {path}")
-    st.write(problem.plotSolution(path))
-
-
-if __name__ == '__main__':
-    main()
+st.title("Breadth-first search")
+problem = GridSearchProblem()
+st.write(f"Start: {problem.start}")
+st.write(f"Goal: {problem.goal}")
+path = breadth_first_search(problem)
+st.write(f"Path: {path}")
+st.write(problem.plotSolution(path))
