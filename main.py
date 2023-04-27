98ce65df54889bb9acb4c9ece154b7cdfe0dca25
Brandon Corfman <bcorfman@fastmail.fm>
2023-04-26 22:45:34 -0400

    Finished text version

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import streamlit as st
 
 from core.search import GridSearchProblem, breadth_first_search
 
+st.title("Breadth-first search")
+problem = GridSearchProblem()
+st.sidebar.title("Parameters")
+label_col1, label_col2 = st.sidebar.columns(2, gap="small")
+label_col1.caption("Start")
+label_col2.caption("Goal")
+col1, col2, col3, col4 = st.sidebar.columns(4, gap="small")
+txt_start_x = col1.text_input("X:", value=problem.start[1])
+txt_start_y = col2.text_input("Y:", value=problem.start[0])
+txt_goal_x = col3.text_input("X:", value=problem.goal[1])
+txt_goal_y = col4.text_input("Y:", value=problem.goal[0])
 
-def main():
-    st.title("Breadth-first search")
-    problem = GridSearchProblem()
-    st.write(f"Start: {problem.start}")
-    st.write(f"Goal: {problem.goal}")
-    path = breadth_first_search(problem)
-    st.write(f"Path: {path}")
-    st.write(problem.plotSolution(path))
-
-
-if __name__ == "__main__":
-    main()
+new_start = int(txt_start_y), int(txt_start_x)
+new_goal = int(txt_goal_y), int(txt_goal_x)
+problem = GridSearchProblem(start=new_start, goal=new_goal)
+path = breadth_first_search(problem)
+st.text(problem.plotSolution(path))
+st.write(f"Path: {path}")
