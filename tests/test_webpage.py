4910bac2b1189cdedb75db6f71030bf15065b68c
Brandon Corfman <bcorfman@fastmail.fm>
2023-05-01 13:16:34 +0000

    modify title test

--- tests/test_webpage.py
+++ tests/test_webpage.py
@@ -1,7 +1,9 @@
+import re
+
 from playwright.sync_api import Page, expect
 
 
 def test_initial_page_has_correct_path_and_details(page: Page):
     page.goto('https://bcorfman-bfs-main-ihgp7e.streamlit.app/')
 
-    expect(page).to_have_title("Breadth-first search: Streamlit")
+    expect(page).to_have_title(re.compile("Breadth-first search"))
