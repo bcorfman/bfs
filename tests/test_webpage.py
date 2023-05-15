6d55ae7f9d9a6242e68789c404071bf7ffcf1aad
Brandon Corfman <bcorfman@fastmail.fm>
2023-05-15 08:56:42 -0400

    Introduce docker compose

--- tests/test_webpage.py
+++ tests/test_webpage.py
@@ -1,21 +1,20 @@
 import os
 
 import pytest
-from playwright.async_api import Page, expect
+from playwright.sync_api import Page, expect
 
 
 @pytest.mark.system
-@pytest.mark.asyncio_cooperative
-async def test_default_solution_path(page: Page):
+def test_default_solution_path(page: Page):
     url = os.environ.get('STREAMLIT_DEPLOYMENT_URL')
     if url:
-        await page.goto(url)
+        page.goto(url)
     else:
-        await page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
+        page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
     locator = page.frame_locator("iframe[title=\"streamlitApp\"]")
-    element = await locator.get_by_text("Path:")
+    element = locator.get_by_text("Path:")
     assert element is not None
-    await expect(element).to_contain_text(
+    expect(element).to_contain_text(
         "[(15, 6), (15, 7), (15, 8), (16, 8), (17, 8), (18, 8), (19, 8), "
         "(20, 8), (21, 8), (22, 8), (23, 8), (24, 8), (25, 8), (26, 8), "
         "(27, 8), (28, 8), (29, 8), (30, 8), (31, 8), (32, 8), (33, 8), "
@@ -24,39 +23,35 @@ async def test_default_solution_path(page: Page):
 
 
 @pytest.mark.system
-@pytest.mark.asyncio_cooperative
-async def test_start_node_outside_map_boundary(page: Page):
+def test_start_node_outside_map_boundary(page: Page):
     url = os.environ.get('STREAMLIT_DEPLOYMENT_URL')
     if url:
-        await page.goto(url)
+        page.goto(url)
     else:
-        await page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
+        page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
 
     locator = page.frame_locator("iframe[title=\"streamlitApp\"]")
-    await locator.get_by_role("spinbutton", name="X:").first.fill("30")
-    await locator.get_by_role("spinbutton", name="X:").first.press("Tab")
-    await locator.get_by_role("spinbutton", name="Y:").first.fill("5")
-    await locator.get_by_role("spinbutton", name="Y:").first.press("Tab")
-    element = await locator.get_by_text("Path:")
+    locator.get_by_role("spinbutton", name="X:").first.fill("30")
+    locator.get_by_role("spinbutton", name="X:").first.press("Tab")
+    locator.get_by_role("spinbutton", name="Y:").first.fill("5")
+    locator.get_by_role("spinbutton", name="Y:").first.press("Tab")
+    element = locator.get_by_text("Path:")
     assert element is not None
-    await expect(element).to_contain_text(
-        "No solution found. Outside map boundary.")
+    expect(element).to_contain_text("No solution found. Outside map boundary.")
 
 
 @pytest.mark.system
-@pytest.mark.asyncio_cooperative
-async def test_start_node_one_line_above_map_boundary(page: Page):
+def test_start_node_one_line_above_map_boundary(page: Page):
     url = os.environ.get('STREAMLIT_DEPLOYMENT_URL')
     if url:
-        await page.goto(url)
+        page.goto(url)
     else:
-        await page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
+        page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
     locator = page.frame_locator("iframe[title=\"streamlitApp\"]")
-    await locator.get_by_role("spinbutton", name="X:").first.fill("26")
-    await locator.get_by_role("spinbutton", name="X:").first.press("Tab")
-    await locator.get_by_role("spinbutton", name="Y:").first.fill("6")
-    await locator.get_by_role("spinbutton", name="Y:").first.press("Tab")
-    element = await locator.get_by_text("Path:")
+    locator.get_by_role("spinbutton", name="X:").first.fill("26")
+    locator.get_by_role("spinbutton", name="X:").first.press("Tab")
+    locator.get_by_role("spinbutton", name="Y:").first.fill("6")
+    locator.get_by_role("spinbutton", name="Y:").first.press("Tab")
+    element = locator.get_by_text("Path:")
     assert element is not None
-    await expect(element).to_contain_text(
-        "No solution found. Outside map boundary.")
+    expect(element).to_contain_text("No solution found. Outside map boundary.")
