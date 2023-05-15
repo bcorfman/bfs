6fcebaa96fff9f185769a405d5bee8ddf1ec4752
Brandon Corfman <bcorfman@fastmail.fm>
2023-05-15 19:23:37 -0400

    try adding DISPLAY to env

--- tests/test_webpage.py
+++ tests/test_webpage.py
@@ -1,7 +1,7 @@
 import os
 
 import pytest
-from playwright.sync_api import Page  # , expect
+from playwright.sync_api import Page, expect
 
 
 @pytest.mark.system
@@ -11,16 +11,15 @@ def test_default_solution_path(page: Page):
         page.goto(url)
     else:
         page.goto("https://bcorfman-bfs-main-ihgp7e.streamlit.app/")
-    assert page.content() is None
-    # locator = page.frame_locator("iframe[title=\"streamlitApp\"]")
-    # element = locator.get_by_text("Path:")
-    # assert element is not None
-    # expect(element).to_contain_text(
-    #    "[(15, 6), (15, 7), (15, 8), (16, 8), (17, 8), (18, 8), (19, 8), "
-    #    "(20, 8), (21, 8), (22, 8), (23, 8), (24, 8), (25, 8), (26, 8), "
-    #    "(27, 8), (28, 8), (29, 8), (30, 8), (31, 8), (32, 8), (33, 8), "
-    #    "(34, 8), (35, 8), (36, 8), (37, 8), (38, 8), (39, 8), (39, 7), "
-    #    "(39, 6), (40, 6), (41, 6), (42, 6), (42, 7), (43, 7), (44, 7)]")
+    locator = page.frame_locator("iframe[title=\"streamlitApp\"]")
+    element = locator.get_by_text("Path:")
+    assert element is not None
+    expect(element).to_contain_text(
+        "[(15, 6), (15, 7), (15, 8), (16, 8), (17, 8), (18, 8), (19, 8), "
+        "(20, 8), (21, 8), (22, 8), (23, 8), (24, 8), (25, 8), (26, 8), "
+        "(27, 8), (28, 8), (29, 8), (30, 8), (31, 8), (32, 8), (33, 8), "
+        "(34, 8), (35, 8), (36, 8), (37, 8), (38, 8), (39, 8), (39, 7), "
+        "(39, 6), (40, 6), (41, 6), (42, 6), (42, 7), (43, 7), (44, 7)]")
 
 
 """ @pytest.mark.system
