238977e2ff2af9cfbcf9685533ab3fd5eeaf5b68
Brandon Corfman <bcorfman@fastmail.fm>
2023-05-17 23:08:18 -0400

    move all number input widgets later

--- main.py
+++ main.py
@@ -16,47 +16,42 @@ st.sidebar.title("Parameters")
 label_col11, label_col12 = st.sidebar.columns(2, gap="small")
 label_col11.caption("Start")
 col11, col12 = st.sidebar.columns(2, gap="small")
+# create a placeholder so we can redefine the Y input later once
+# the map height is known
+start_x_placeholder = col11.empty()
+start_y_placeholder = col12.empty()
+
+label_col21, label_col22 = st.sidebar.columns(2, gap="small")
+label_col21.caption("Goal")
+col21, col22 = st.sidebar.columns(2, gap="small")
+# create a placeholder so we can redefine the Y input later once
+# the map height is known
+goal_x_placeholder = col21.empty()
+goal_y_placeholder = col22.empty()
+
+# pulling start and goal coordinates from the text boxes since the
+# number widgets prevent input errors from occurring
+new_start = int(problem.start[0]), int(problem.start[1])
+new_goal = int(problem.goal[0]), int(problem.goal[1])
+problem = GridSearchProblem(start=new_start, goal=new_goal)
+soln = breadth_first_search(problem)
+# revise max Y inputs to actual grid height once it's been read in
+grid_height = problem.getGridHeight()
 txt_start_x = col11.number_input("X:",
                                  format="%d",
                                  min_value=1,
                                  max_value=MAX_GRID_WIDTH,
                                  value=problem.start[0])
-# create a placeholder so we can redefine the Y input later once
-# the map height is known
-start_y_placeholder = col12.empty()
-txt_start_y = start_y_placeholder.number_input("Y:",
-                                               format="%d",
-                                               min_value=1,
-                                               value=problem.start[1])
-label_col21, label_col22 = st.sidebar.columns(2, gap="small")
-label_col21.caption("Goal")
-col21, col22 = st.sidebar.columns(2, gap="small")
-txt_goal_x = col21.number_input("X:",
-                                format="%d",
-                                min_value=1,
-                                max_value=MAX_GRID_WIDTH,
-                                value=problem.goal[0])
-# create a placeholder so we can redefine the Y input later once
-# the map height is known
-goal_y_placeholder = col22.empty()
-txt_goal_y = goal_y_placeholder.number_input("Y:",
-                                             format="%d",
-                                             min_value=1,
-                                             value=problem.goal[1])
-
-# pulling start and goal coordinates from the text boxes since the
-# number widgets prevent input errors from occurring
-new_start = int(txt_start_x), int(txt_start_y)
-new_goal = int(txt_goal_x), int(txt_goal_y)
-problem = GridSearchProblem(start=new_start, goal=new_goal)
-soln = breadth_first_search(problem)
-# revise max Y inputs to actual grid height once it's been read in
-grid_height = problem.getGridHeight()
 txt_start_y = start_y_placeholder.number_input("Y:",
                                                format="%d",
                                                min_value=1,
                                                max_value=grid_height + 1,
                                                value=problem.start[1])
+txt_goal_x = goal_x_placeholder.number_input("X:",
+                                             format="%d",
+                                             min_value=1,
+                                             max_value=MAX_GRID_WIDTH,
+                                             value=problem.goal[0])
 txt_goal_y = goal_y_placeholder.number_input("Y:",
                                              format="%d",
                                              min_value=1,
